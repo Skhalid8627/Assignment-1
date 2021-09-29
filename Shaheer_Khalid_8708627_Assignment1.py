deb356869b86f482da43857be3b959868e20a3ed
Shaheer Khalid <skhalid8627@conestogac.on.ca>
2021-09-29 02:48:55 +0000

    Third commit - Still working on Receipt|formatting

--- Shaheer_Khalid_8708627_Assignment1.py
+++ Shaheer_Khalid_8708627_Assignment1.py
@@ -1,12 +1,12 @@
 #Welcome Message
 print("WELCOME TO ABBY'S MERCHANDIZING 'A.M' ONLINE STORE")
-print("***************************************************************************")
+print("---------------------------------------------------------------------------")
 print("")
 print("WE SELL HIGH QUALITY T-SHIRTS WITH AN INCREDIBLE PRICE OF ONLY $9.99 EACH")
 
 #Get Colour of shirt (I'll ask for 4 different colours)
-print("***************************************************************************")
-print("WE HAVE A VARIETY OF COLOURS TO CHOOSE FROM")
+print("---------------------------------------------------------------------------")
+print("THIS OUR CATALOG OF COLOURS TO CHOOSE FROM")
 print("Please enter 'W' for WHITE")
 print("Please enter 'B' for BLUE")
 print("Please enter 'O' for ORANGE")
@@ -25,35 +25,42 @@ elif (colourofshirt == "R") or (colourofshirt == "r"):
     colourofshirt = "RED"
 
 #Get type of shirt (polo or t-shirt )
-print("***************************************************************************")
+print("---------------------------------------------------------------------------")
 print("WE OFFER TWO OPTIONS FOR OUR T-SHIRTS:")
 print("1.POLO SHIRT        2.REGULAR T_SHIRT")
 print("Please enter 1 for POLO SHIRT OR enter 2 for REGULAR T-SHIRT")
 shirttype = float(input())
 
+#As required user will make their choice by either entering 1 or 2
 if (shirttype == 1):
     shirttype = "POLO SHIRT"
 elif (shirttype == 2):
     shirttype = "REGULAR T-SHIRT"
 
 #Get quantity of shirt
-print("***************************************************************************")
+print("---------------------------------------------------------------------------")
 print("HOW MANY SHIRTS WOULD YOU LIKE TO PURCHASE TODAY?")
 shirtqty = float(input())
 
 
 #Declare cost of shirt (CONSTANT)
 COST = 9.99
-#Calculate the total cost with HST
+#Calculate the total cost with HST13%
 tax = (shirtqty * COST) * 0.13
 totalcostbeftax = COST * shirtqty
 totalcostafttax = (shirtqty * COST) + tax
 
 #Print receipt/Summary of the order
 #testing
-print(colourofshirt)
-print(shirttype)
-print(shirtqty)
-print(totalcostbeftax)
-print(totalcostafttax)
+print("----------------------------RECIEPT-----------------------------")
+print("----------------------ABBY'S MURCHANDIZING----------------------")
+print("")
+print("COLOUR OF SHIRT       "+ colourofshirt)
+
+print("YOUR CHOICE OF SHIRT: "+ shirttype)
+print("QTY                   "+ shirtqty)
+print("                      -----------------")
+print("SUB-TOTAL            $"+ totalcostbeftax)
+print("HST 13%              $"+ tax)
+print("TOTAL COST           $"+ totalcostafttax)
 
