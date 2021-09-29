1567b5716dcaab54bb5b8590f7002161ba62aa48
Shaheer Khalid <skhalid8627@conestogac.on.ca>
2021-09-29 01:26:38 +0000

    Second commit - Calculations and more

--- Shaheer_Khalid_8708627_Assignment1.py
+++ Shaheer_Khalid_8708627_Assignment1.py
@@ -1,15 +1,16 @@
 #Welcome Message
-print("WELCOME TO ABBY'S MERCHANDIZING ONLINE STORE")
-print("*********************************************")
+print("WELCOME TO ABBY'S MERCHANDIZING 'A.M' ONLINE STORE")
+print("***************************************************************************")
 print("")
-print("WE SELL HIGH QUALITY T-SHIRTS WITH AN AMAIZING PRICE OF $9.99")
+print("WE SELL HIGH QUALITY T-SHIRTS WITH AN INCREDIBLE PRICE OF ONLY $9.99 EACH")
 
 #Get Colour of shirt (I'll ask for 4 different colours)
+print("***************************************************************************")
 print("WE HAVE A VARIETY OF COLOURS TO CHOOSE FROM")
-print("Please press 'W' for WHITE")
-print("Please press 'B' for BLUE")
-print("Please press 'O' for ORANGE")
-print("Please press 'R' for RED")
+print("Please enter 'W' for WHITE")
+print("Please enter 'B' for BLUE")
+print("Please enter 'O' for ORANGE")
+print("Please enter 'R' for RED")
 print("WHAT COLOUR WOULD YOU LIKE TO PICK TODAY ?")
 colourofshirt = str(input())
 #Using if statement to assign my variable a colour which could also be printed later
@@ -22,14 +23,37 @@ elif (colourofshirt == "O") or (colourofshirt == "o"):
     colourofshirt = "ORANGE"
 elif (colourofshirt == "R") or (colourofshirt == "r"):
     colourofshirt = "RED"
-print(colourofshirt)
 
 #Get type of shirt (polo or t-shirt )
+print("***************************************************************************")
+print("WE OFFER TWO OPTIONS FOR OUR T-SHIRTS:")
+print("1.POLO SHIRT        2.REGULAR T_SHIRT")
+print("Please enter 1 for POLO SHIRT OR enter 2 for REGULAR T-SHIRT")
+shirttype = float(input())
+
+if (shirttype == 1):
+    shirttype = "POLO SHIRT"
+elif (shirttype == 2):
+    shirttype = "REGULAR T-SHIRT"
 
 #Get quantity of shirt
+print("***************************************************************************")
+print("HOW MANY SHIRTS WOULD YOU LIKE TO PURCHASE TODAY?")
+shirtqty = float(input())
 
-#Declare cost of shirt
 
+#Declare cost of shirt (CONSTANT)
+COST = 9.99
 #Calculate the total cost with HST
+tax = (shirtqty * COST) * 0.13
+totalcostbeftax = COST * shirtqty
+totalcostafttax = (shirtqty * COST) + tax
 
 #Print receipt/Summary of the order
+#testing
+print(colourofshirt)
+print(shirttype)
+print(shirtqty)
+print(totalcostbeftax)
+print(totalcostafttax)
+
